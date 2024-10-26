de826c42e1b73b7f3580c1db1a9bac64ef7f0b35
unknown <ny5965364@gmail.com>
2024-10-26 23:01:38 +0530

    my new portfolio

---
+++ n.js
@@ -0,0 +1,5 @@
+// You can add interactivity here, such as form validation or project filters
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Portfolio Loaded Successfully");
+  });
+  
